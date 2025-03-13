68389fe2136ea2a72c4c81fb7f5e59c42425d70d
pealmoor <pealmo98@gmail.com>
2025-03-12 22:12:30 -0500

    Disparos agg

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ pygame.display.set_caption("Shooter 2D")
 # Colores
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
+RED = (255, 0, 0)
 
 # Reloj para controlar los FPS
 clock = pygame.time.Clock()
@@ -34,10 +35,23 @@ class Player:
     def draw(self, screen):
         pygame.draw.rect(screen, WHITE, self.rect)
 
+# Clase para los disparos
+class Bullet:
+    def __init__(self, x, y):
+        self.rect = pygame.Rect(x + 20, y, 10, 20)
+        self.speed = -7
+    
+    def move(self):
+        self.rect.y += self.speed
+    
+    def draw(self, screen):
+        pygame.draw.rect(screen, RED, self.rect)
+
 # Bucle principal
 def main():
     running = True
     player = Player(WIDTH // 2, HEIGHT - 60)
+    bullets = []
     
     while running:
         screen.fill(BLACK)  # Limpiar pantalla
@@ -45,10 +59,21 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    bullets.append(Bullet(player.rect.x, player.rect.y))
         
         keys = pygame.key.get_pressed()
         player.move(keys)
+        
+        for bullet in bullets[:]:
+            bullet.move()
+            if bullet.rect.y < 0:
+                bullets.remove(bullet)
+        
         player.draw(screen)
+        for bullet in bullets:
+            bullet.draw(screen)
         
         pygame.display.flip()  # Actualizar pantalla
         clock.tick(60)  # Limitar a 60 FPS
